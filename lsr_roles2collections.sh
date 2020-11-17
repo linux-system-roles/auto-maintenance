5b4c617be67385df7c9dae5d37ce4aa1f4d51920
Rich Megginson <rmeggins@redhat.com>
2020-11-17 16:27:37 -0700

    Collection release scripts, data, docs

--- lsr_roles2collections.sh
+++ lsr_roles2collections.sh
@@ -2,6 +2,10 @@
 
 COLLECTION_SRC_PATH=${COLLECTION_SRC_PATH:-"${HOME}/linux-system-roles"}
 COLLECTION_DEST_PATH=${COLLECTION_DEST_PATH:-"${HOME}/.ansible/collections"}
+COLLECTION_SRC_OWNER=${COLLECTION_SRC_OWNER:-"linux-system-roles"}
+COLLECTION_NAMESPACE=${COLLECTION_NAMESPACE:-"fedora"}
+COLLECTION_NAME=${COLLECTION_NAME:-"linux_system_roles"}
+
 ROLES=${ROLES:-"certificate kdump kernel_settings logging metrics nbde_client nbde_server network selinux storage timesync tlog tuned"}
 CWD=$(pwd)
 
@@ -26,7 +30,7 @@ do
         git pull
     fi
     cd "$CWD" || exit
-    python lsr_role2collection.py --role "$role"
+    python lsr_role2collection.py --role "$role" --src-owner "$COLLECTION_SRC_OWNER" --namespace "$COLLECTION_NAMESPACE" --collection "$COLLECTION_NAME"
 done
 role="template"
 if [ ! -d "$COLLECTION_SRC_PATH/$role" ]; then

20d31bf5d8e7eb67ce48af39e36c9f79d87490e3
pcahyna <pcahyna@users.noreply.github.com>
2021-02-11 22:30:49 +0100

    Add a script to transform galaxy.yml (#46)
    Replaces collection namespace, name and version, using ruamel.yaml
    
    thanks @richm for the suggestions

---
+++ galaxy_transform.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# Changes namespace, name, and version in Galaxy metadata.
+# Useful for releasing to Automation Hub, where Collections live
+# in namespaces separated from Ansible Galaxy.
+
+import sys
+from ruamel.yaml import YAML
+
+filepath = "galaxy.yml"
+
+buf = open(filepath).read()
+
+yaml = YAML(typ="rt")
+yaml.default_flow_style = False
+yaml.preserve_quotes = True
+yaml.indent(mapping=2, sequence=4, offset=2)
+
+code = yaml.load(buf)
+
+code["namespace"] = sys.argv[1]
+code["name"] = sys.argv[2]
+code["version"] = sys.argv[3]
+yaml.dump(code, sys.stdout)

1bab1d0040bb2fd35a4efd386ec52500e2ddd49d
Noriko Hosoi <nhosoi@redhat.com>
2021-04-18 22:59:56 -0700

    Fixing check_rpmspec_collection.sh
    Adjusting the expected behaviour based on the comment
    https://src.fedoraproject.org/rpms/linux-system-roles/pull-request/4#comment-72412.

--- check_rpmspec_collection.sh
+++ check_rpmspec_collection.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo "Description - Verify rpm package count with various combination of ansible and collection_artifact options."
-    echo "            - Check there is no README.html files in /usr/share/andible/collections."
+    echo "            - Check there is no README.html files in /usr/share/ansible/collections."
     echo "Usage: $0 [ -h | --help ] | [ fedpkg | rhpkg [ branch_name ] ]"
     exit 0
 fi
@@ -24,7 +24,8 @@ run() {
     output=/tmp/$pkgcmd.$ext
     cmdline="$pkgcmd --release $branch local $option"
     echo "$cmdline"
-    $cmdline > "$output" 2>&1
+    mkdir noarch
+    $cmdline > "$output" 2>&1 || :
     count=$( find noarch -name "*.rpm" | wc -l )
     if [ "$count" -eq "$expect" ]; then
       echo OK - result "$count" is "$expect"
@@ -41,11 +42,11 @@ run() {
 }
 
 run "--without ansible" "without_ansible" 1
-run "--without ansible --with collection_artifact" "without_ansible-with_collection_artifact" 1
+run "--without ansible --with collection_artifact" "without_ansible-with_collection_artifact" 0
 run "--without ansible --without collection_artifact" "without_ansible-without_collection_artifact" 1
 run "--without collection_artifact" "without_collection_artifact" 1
-run "--with ansbile --without collection_artifact" "with_ansible-without_collection_artifact" 1
+run "--with ansible --without collection_artifact" "with_ansible-without_collection_artifact" 1
 run "" "no_opt" 1
-run "--with ansbile" "with_ansible" 1
+run "--with ansible" "with_ansible" 1
 run "--with collection_artifact" "with_collection_artifact" 2
 run "--with ansible --with collection_artifact" "with_ansible-with_collection_artifact" 2

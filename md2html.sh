f3d3442e51bf0f3ec82957fe5befe78bee5d6ba8
Sergei Petrosian <spetrosi@redhat.com>
2021-05-05 15:10:23 +0200

    use kramdown to convert directly to HTML

--- md2html.sh
+++ md2html.sh
@@ -16,27 +16,33 @@ while getopts "hl" opt; do
 done
 
 for file in "$@"; do
-        md2adoc_tool=""
-        # RHEL 9 in brew cannot use pandoc, hence trying kramdoc first
-        if type -p kramdown >/dev/null; then
-          md2adoc_tool=kramdown
-        elif type -p pandoc >/dev/null; then
-          md2adoc_tool=pandoc
+        md2html_tool=""
+        # RHEL 9 in brew cannot use pandoc, hence trying kramown first
+        if rpm -q rubygem-kramdown-parser-gfm > /dev/null; then
+          md2html_tool=kramdown
+        elif rpm -q pandoc >/dev/null; then
+          md2html_tool=pandoc
         fi
-        if [ $md2adoc_tool == kramdown ]; then
-          kramdoc --format=GFM --output="${file%.md}.tmp.adoc" "${file}"
-        elif [ $md2adoc_tool == pandoc ]; then
-          pandoc -f markdown_github "${file}" -t asciidoc -o "${file%.md}.tmp.adoc"
+
+        # With kramdown, convert directly to HTML
+        if [ $md2html_tool == kramdown ]; then
+          $md2html_tool --extension parser-gfm --input GFM --output html "${file}" > "${file%.md}.html"
+          if [ "$convert_link" -ne 0 ]; then
+                  sed -i -e "s/\.md\>/\.html/g" "${file%.md}.html"
+          fi
+
+        # With pandoc, convert to adoc, then to HTML
+        elif [ $md2html_tool == pandoc ]; then
+          $md2html_tool -f markdown_github "${file}" -t asciidoc -o "${file%.md}.tmp.adoc"
+          if [ "$convert_link" -ne 0 ]; then
+                  sed -i -e "s/\.md\>/\.html/g" "${file%.md}.tmp.adoc"
+          fi
+          touch -r "${file}" "${file%.md}.tmp.adoc"
+          TZ=UTC asciidoc -o "${file%.md}.html" -a footer-style=none -a toc2 -a source-highlighter=highlight "${file%.md}.tmp.adoc"
+          tr -d '\r' < "${file%.md}.html" > "${file%.md}.tmp.adoc"
+          mv "${file%.md}.tmp.adoc" "${file%.md}.html"
         else
           echo Cannot find a tool to convert md to adoc
           exit 1
         fi
-
-	if [ "$convert_link" -ne 0 ]; then
-		sed -i -e "s/\.md\>/\.html/g" "${file%.md}.tmp.adoc"
-	fi
-	touch -r "${file}" "${file%.md}.tmp.adoc"
-	TZ=UTC asciidoc -o "${file%.md}.html" -a footer-style=none -a toc2 -a source-highlighter=highlight "${file%.md}.tmp.adoc"
-	tr -d '\r' < "${file%.md}.html" > "${file%.md}.tmp.adoc"
-	mv "${file%.md}.tmp.adoc" "${file%.md}.html"
 done

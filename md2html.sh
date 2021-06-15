6b13c9c8f7351bf29d54f04390a9434e1a82ffb7
Rich Megginson <rmeggins@redhat.com>
2021-06-15 14:49:59 -0600

    pandoc - use gfm instead of markdown
    pandoc should use `gfm` instead of `markdown_github`.  This makes the
    internal links in the HTML work when converting the resulting
    asciidoc to HTML.
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1962976

--- md2html.sh
+++ md2html.sh
@@ -34,7 +34,7 @@ for file in "$@"; do
     --output html "${file}" > "${file%.md}.html"
   # With pandoc, convert to adoc, then to HTML
   elif [ "$md2html_tool" == pandoc ]; then
-    $md2html_tool -f markdown_github "${file}" -t asciidoc -o "${file%.md}.tmp.adoc"
+    $md2html_tool -f gfm "${file}" -t asciidoc -o "${file%.md}.tmp.adoc"
     touch -r "${file}" "${file%.md}.tmp.adoc"
     TZ=UTC asciidoc -o "${file%.md}.html" -a footer-style=none -a toc2 -a source-highlighter=highlight "${file%.md}.tmp.adoc"
     tr -d '\r' < "${file%.md}.html" > "${file%.md}.tmp.adoc"

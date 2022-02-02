77d478c5671d9defc1ddab525d09cbc56c537633
Sergei Petrosian <30409084+spetrosi@users.noreply.github.com>
2022-02-02 11:34:45 +0100

    [WIP] Improve update_vendored_collections.py and prepare to run in container (#121)
    * Add --anonymous for `centpkg clone` to clone over HTTPS
    
    * Remove repo directories before cloning to avoid conflicts
    
    * Remove target-os argument, add centpkg_cmd and fedpkg_cmd
    
    * Change move_tarballs_to_repo to copy_tarballs_to_repo
    
    * Fix replace_sources_in_spec to write the file after replace
    
    * Remove the branch argument, use centos_repo and fedora_repo
    
    * Remove upload_sources, not required for scratch build
    
    * Remove tarballs after script execution
    
    * Add work on fedpkg linux-system-roles repository
    
    * Add the delete_files function
    
    * Improve update_spec to add changelog entry and ready file properly
    
    * Push spec with new tarballs to Fedora
    
    * Configure git credentials, refractor, add comments
    
    * Add updating vendored_collections.yml
    
    * Add pushing to GitHub
    
    * Print different commit messages for Fedora and GitHub
    
    * Do not add remote if exists, checkout anyway if branch exists
    
    * Do not commit tarballs to GitHub, remove function that deletes files
    
    * Remove ansible.netcommon because rhel-system-roles do not vendore it

--- update_vendored_collections/update_vendored_collections.py
+++ update_vendored_collections/update_vendored_collections.py
@@ -8,17 +8,7 @@ import subprocess
 import sys
 import yaml
 import glob
-
-
-def set_rpkg_cmd(target_os):
-    if target_os == "centos-stream":
-        return "centpkg"
-    elif target_os == "rhel":
-        return "rhpkg"
-    else:
-        raise NameError(
-            "The --target-os argument must be set to either centos-stream or rhel"
-        )
+import datetime
 
 
 def run_cmd(cmd, cwd):
@@ -63,10 +53,15 @@ def get_updated_collection_tarball(coll):
         return {coll_name: coll_tar}
 
 
-def clone_repo(rpkg_cmd, branch, repo):
+def clone_repo(repo, branch, rpkg_cmd):
+    if os.path.exists(repo):
+        print(f"{repo} directory already exists, removing")
+        shutil.rmtree(repo)
+    # --anonymous is required to clone over HTTPS and avoid SSH authentication
     cmd = [
         rpkg_cmd,
         "clone",
+        "--anonymous",
         repo,
         "--branch",
         branch,
@@ -77,24 +72,47 @@ def clone_repo(rpkg_cmd, branch, repo):
     run_cmd(cmd, os.path.curdir)
 
 
-def move_tarballs_to_repo(collection_tarballs, repo):
-    print(f"Moving {', '.join(collection_tarballs.values())} to {repo}")
+def copy_tarballs_to_repo(collection_tarballs, repo):
+    print(f"Copying {', '.join(collection_tarballs.values())} to {repo}")
     for tarball in collection_tarballs.values():
-        shutil.move(os.path.abspath(tarball), os.path.join(repo, tarball))
+        shutil.copy(os.path.abspath(tarball), os.path.join(repo, tarball))
 
 
-def upload_sources(rpkg_cmd, collection_tarballs, repo):
-    print(f"Uploading {', '.join(collection_tarballs.values())} sources to {repo}")
-    cmd = [rpkg_cmd, "upload", *collection_tarballs.values()]
-    run_cmd(cmd, repo)
-
-
-def replace_sources_in_spec(collection_tarballs, repo):
+def spec_replace_sources(content, collection_tarballs):
     print("Replacing sources in the spec file")
     for collection, tarball in collection_tarballs.items():
-        with open(os.path.join(repo, "linux-system-roles.spec"), "r") as f:
-            content = f.read()
-            re.sub(collection + ".*$", tarball, content, flags=re.M)
+        content = re.sub(
+            collection.replace(".", "-") + ".*$", tarball, content, flags=re.MULTILINE
+        )
+    return content
+
+
+def spec_add_changelog(content, collection_tarballs, lines):
+    print("Adding changelog entry to the spec file")
+    for line in lines:
+        if line == "%changelog\n":
+            current_version = re.sub(".*> - ", "", next(lines)).strip()
+            new_version = current_version[:-1] + str(
+                int(re.sub(".*-", "", current_version)) + 1
+            )
+            changelog_date = datetime.datetime.now().strftime("%a %b %d %Y")
+            meta_line = f"* {changelog_date} Sergei Petrosian <spetrosi@redhat.com> - {new_version}\n"
+            comment_line = f"- Update {', '.join(collection_tarballs.keys())}\n\n"
+            content = re.sub(
+                "%changelog\n", "%changelog\n" + meta_line + comment_line, content
+            )
+            return content
+
+
+def update_spec(collection_tarballs, repo):
+    with open(os.path.join(repo, "linux-system-roles.spec"), "r") as f:
+        content = f.read()
+        f.seek(0)
+        lines = iter(f.readlines())
+    content = spec_replace_sources(content, collection_tarballs)
+    content = spec_add_changelog(content, collection_tarballs, lines)
+    with open(os.path.join(repo, "linux-system-roles.spec"), "w") as f:
+        f.write(content)
 
 
 def scratch_build(rpkg_cmd, repo):
@@ -110,6 +128,69 @@ def scratch_build(rpkg_cmd, repo):
     return build_url
 
 
+def repo_configure_credentials(repo, repo_user, repo_email):
+    print(f"Configuring the {repo} repository to use {repo_user} credentials")
+    cmd = ["git", "config", "user.name", repo_user]
+    run_cmd(cmd, repo)
+    cmd = ["git", "config", "user.email", repo_email]
+    run_cmd(cmd, repo)
+
+
+def repo_add_remote(repo, repo_user, repo_url):
+    print(f"Adding {repo_user} remote to the {repo} repository")
+    cmd = ["git", "remote"]
+    remotes = run_cmd(cmd, repo)
+    if repo_user not in remotes.stdout:
+        cmd = ["git", "remote", "add", repo_user, repo_url]
+        run_cmd(cmd, repo)
+    else:
+        print(f"Remote {repo_user} already exists, continuing")
+
+
+def repo_commit_changes(repo, commit_message, branch, files_list):
+    print(f"Checking out the {branch} branch")
+    cmd = ["git", "checkout", "-B", branch]
+    run_cmd(cmd, repo)
+    print(f"Staging {', '.join(files_list)}")
+    for file in files_list:
+        cmd = ["git", "add", file]
+        run_cmd(cmd, repo)
+    print("Committing changes")
+    cmd = ["git", "commit", "--message", commit_message]
+    run_cmd(cmd, repo)
+
+
+def repo_push(repo, repo_user, branch):
+    cmd = ["git", "push", repo_user, branch, "--force"]
+    run_cmd(cmd, repo)
+
+
+def update_vendored_collections_yml(hsh, collection_tarballs, requirements):
+    collections_versions = {}
+    updated_requirements = {"collections": []}
+    for collection, tarball in collection_tarballs.items():
+        version = re.sub(".tar.gz", "", re.sub(".*-", "", tarball))
+        collections_versions.update({collection: version})
+    for coll in hsh["collections"]:
+        for collection, version in collections_versions.items():
+            if coll["name"] == collection:
+                updated_requirements["collections"].append(
+                    {"name": coll["name"], "version": version}
+                )
+    print(f"Update {requirements} with fresh collections versions")
+    with open(requirements, "w") as f:
+        yaml.dump(updated_requirements, f)
+
+
+def delete_files(centos_repo, fedora_repo, collection_tarballs):
+    for repo in centos_repo, fedora_repo:
+        print(f"Removing the {repo} repository")
+        shutil.rmtree(repo)
+    print(f"Removing the {', '.join(collection_tarballs.values())} collection tarballs")
+    for tarball in collection_tarballs.values():
+        os.remove(tarball)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -118,39 +199,86 @@ def main():
         default=os.environ.get("REQUIREMENTS_YML", "vendored_collections.yml"),
         help="Path/filename for file containing vendored collections in the requirements.yml format",
     )
-    parser.add_argument(
-        "--target-os",
-        type=str,
-        default=os.environ.get("TARGET_OS", "centos-stream"),
-        help="The target OS to work on. Either centos-stream or rhel.",
-    )
-    parser.add_argument(
-        "--branch",
-        type=str,
-        default=os.environ.get("BRANCH", "c9s"),
-        help="The branch of the repository to work on.",
-    )
 
     args = parser.parse_args()
-    target_os = args.target_os
-    branch = args.branch
-    repo = "rhel-system-roles"
-    rpkg_cmd = set_rpkg_cmd(target_os)
-    hsh = yaml.safe_load(open(args.requirements))
+    centos_repo = "rhel-system-roles"
+    centos_branch = "c9s"
+    fedora_repo = "linux-system-roles"
+    fedora_branch = "rawhide"
+    fedora_user = "linuxsystemroles"
+    fedora_email = "systemroles-owner@lists.fedorahosted.org"
+    fedora_fork_url = "ssh://linuxsystemroles@pkgs.fedoraproject.org/forks/linuxsystemroles/rpms/linux-system-roles.git"
+    fedora_push_branch = "update-vendored-collections"
+    auto_maintenance_repo = sys.path[0]
+    auto_maintenance_user = "tft-bot"
+    auto_maintenance_fork_url = "git@github.com:tft-bot/auto-maintenance.git"
+    auto_maintenance_push_branch = fedora_push_branch
+    centpkg_cmd = "centpkg"
+    fedpkg_cmd = "fedpkg"
+    requirements = args.requirements
+    hsh = yaml.safe_load(open(requirements))
     collection_tarballs = {}
 
     for coll in hsh["collections"]:
         collection_tarballs.update(get_updated_collection_tarball(coll))
     if len(collection_tarballs) != 0:
-        clone_repo(rpkg_cmd, branch, repo)
-        move_tarballs_to_repo(collection_tarballs, repo)
-        upload_sources(rpkg_cmd, collection_tarballs, repo)
-        replace_sources_in_spec(collection_tarballs, repo)
-        build_url = scratch_build(rpkg_cmd, repo)
-        # TODO: Share build_url somewhere
-        print(f"See build progress at {build_url}")
-        print("Cleaning up the downloaded repository")
-        shutil.rmtree(repo)
+        """Make a CentOS scratch build"""
+        clone_repo(centos_repo, centos_branch, centpkg_cmd)
+        copy_tarballs_to_repo(collection_tarballs, centos_repo)
+        update_spec(collection_tarballs, centos_repo)
+        build_url = scratch_build(centpkg_cmd, centos_repo)
+
+        """Push spec file with updated collection tarballs to Fedora"""
+        clone_repo(fedora_repo, fedora_branch, fedpkg_cmd)
+        copy_tarballs_to_repo(collection_tarballs, fedora_repo)
+        update_spec(collection_tarballs, fedora_repo)
+        repo_configure_credentials(fedora_repo, fedora_user, fedora_email)
+        repo_add_remote(fedora_repo, fedora_user, fedora_fork_url)
+        fedora_commit_message = f"""
+Update vendored collections tarballs
+
+The following tarball(s) have updates:
+{', '.join(collection_tarballs.keys())}
+The CentOS scratch build URL:
+{build_url}
+"""
+        repo_commit_changes(
+            fedora_repo,
+            fedora_commit_message,
+            fedora_push_branch,
+            ["linux-system-roles.spec"],
+        )
+        repo_push(fedora_repo, fedora_user, fedora_push_branch)
+
+        """Update vendored_collections.yml and push to GitHub"""
+        update_vendored_collections_yml(hsh, collection_tarballs, requirements)
+        repo_add_remote(
+            auto_maintenance_repo, auto_maintenance_user, auto_maintenance_fork_url
+        )
+        open_pr_url = (
+            "https://src.fedoraproject.org/fork/linuxsystemroles/rpms/linux-system-roles/diff/"
+            "rawhide..update-vendored-collections"
+        )
+        auto_maintenance_commit_message = f"""
+Update {requirements}
+
+The following collection tarball(s) have updates:
+{', '.join(collection_tarballs.keys())}
+The CentOS scratch build URL:
+{build_url}
+AI: @spetrosi to open a PR for linux-system-roles:
+{open_pr_url}
+CC: @rmeggins @nhosoi"
+"""
+        repo_commit_changes(
+            auto_maintenance_repo,
+            auto_maintenance_commit_message,
+            auto_maintenance_push_branch,
+            [requirements],
+        )
+        repo_push(
+            auto_maintenance_repo, auto_maintenance_user, auto_maintenance_push_branch
+        )
 
 
 if __name__ == "__main__":

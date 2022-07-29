0f4548c1fae56a3ad3d8adb3a7b2753390db26aa
Rich Megginson <rmeggins@redhat.com>
2022-07-29 15:41:57 -0600

    add support for creating changelogs for rpm releases
    The function rpm_release will be used to generate the following
    files:
    * cl-md - the new entry to use for CHANGELOG.md
    * cl-spec - the spec file %changelog section update
    * git-commit-msg - the git commit message
    
    The initial CHANGELOG.md is generated using spec_cl_to_cl_md.  Then
    use rpm_release for subsequent releases.

--- bz-manage.sh
+++ bz-manage.sh
@@ -22,6 +22,7 @@ STATUS=${STATUS:-POST}
 BZ_URL=${URL:-https://bugzilla.redhat.com/buglist.cgi}
 LIMIT=${LIMIT:-100}
 BASE_URL="${BZ_URL}?limit=${LIMIT}&component=${COMPONENT}&${ITR_FIELD}=${ITR}"
+show_url="https://bugzilla.redhat.com/show_bug.cgi?id="
 
 setitmdtm() {
   local field val bzlist baseurl
@@ -179,13 +180,193 @@ clone_check() {
   return $rc
 }
 
-case "${1:-}" in
-setitm) setitmdtm itm ;;
-setdtm) setitmdtm dtm ;;
-reset_dev_wb) reset_dev_wb ;;
-get_commit_msg) get_commit_msg ;;
-get_cl) get_cl ;;
-new) shift; new_bz "$@" ;;
-clone_check) clone_check ;;
-*) echo Error - see documentation; exit 1 ;;
-esac
+fmt_summary() {
+  local summary roles
+  summary="$1"
+  roles="$2"
+  if [ -n "$roles" ]; then
+    if [[ "$summary" =~ ^${roles}[\ -]+(.+)$ ]]; then
+      echo "$roles - ${BASH_REMATCH[1]}"
+    else
+      echo "$roles - $summary"
+    fi
+  else
+    echo "$summary"
+  fi
+}
+
+rpm_release() {
+  # look up BZ for given ITR and status (default status is POST)
+  # input is new version - N-V-R format
+  # generate 3 files
+  # - cl-spec - the new %changelog entry for the spec file - user will
+  #   need to edit for name, email
+  # - git-commit-msg - in the format required by dist-git
+  # - cl-md - the new entry for CHANGELOG.md
+  version="$1"; shift
+  local queryurl jq bz summary itr sum_prefix bz_prefix
+  queryurl="${BASE_URL}&bug_status=${STATUS}"
+  jq='.bugs[] | ((.id|tostring) + "|" + (.whiteboard|gsub("role:"; "")) + "|" + .cf_doc_type + "|" + .summary)'
+  datesec=$(date +%s)
+  get_bzs "${queryurl}" "$jq" -r | sort -k 2 -t \| > bzs.raw
+  cat > "${CL_MD:-cl-md}" <<EOF
+[$version] - $(date -I --date=@"$datesec")
+----------------------------
+
+### New Features
+
+EOF
+  echo "* $(LANG=en_US.utf8 date --date=@"$datesec" "+%a %b %d %Y") Your Name <email@redhat.com> - $version" > "${CL_SPEC:-cl-spec}"
+  echo "Package update" > "${GIT_COMMIT_MSG:-git-commit-msg}"
+  new_features=false
+  while IFS=\| read -r bz roles doc_text summary; do
+    fix_summary=$(fmt_summary "$summary" "$roles")
+    if [ "$doc_text" = Enhancement ]; then
+      echo "- [${fix_summary}](${show_url}$bz)" >> "${CL_MD:-cl-md}"
+      echo "- Resolves:rhbz#${bz} : ${fix_summary}" >> "${CL_SPEC:-cl-spec}"
+      { echo ""
+        echo "$fix_summary"
+        echo "Resolves:rhbz#${bz}"; } >> "${GIT_COMMIT_MSG:-git-commit-msg}"
+      new_features=true
+    fi
+  done < bzs.raw
+  if [ "$new_features" = false ]; then
+    echo "- none" >> "${CL_MD:-cl-md}"
+  fi
+  echo "" >> "${CL_MD:-cl-md}"
+  { echo "### Bug Fixes"; echo ""; } >> "${CL_MD:-cl-md}"
+  fixes=false
+  while IFS=\| read -r bz roles doc_text summary; do
+    if [ "$doc_text" = "Bug Fix" ]; then
+      echo "- [${fix_summary}](${show_url}$bz)" >> "${CL_MD:-cl-md}"
+      echo "- Resolves:rhbz#${bz} : ${fix_summary}" >> "${CL_SPEC:-cl-spec}"
+      { echo ""
+        echo "$fix_summary"
+        echo "Resolves:rhbz#${bz}"; } >> "${GIT_COMMIT_MSG:-git-commit-msg}"
+      fixes=true
+    fi
+  done < bzs.raw
+  if [ "$fixes" = false ]; then
+    { echo "- none"; echo ""; } >> "${CL_MD:-cl-md}"
+  fi
+  while IFS=\| read -r bz roles doc_text summary; do
+    if [ "$doc_text" != Enhancement ] && [ "$doc_text" != "Bug Fix" ]; then
+      echo "- Resolves:rhbz#${bz} : ${fix_summary}" >> "${CL_SPEC:-cl-spec}"
+      { echo ""
+        echo "$fix_summary"
+        echo "Resolves:rhbz#${bz}"; } >> "${GIT_COMMIT_MSG:-git-commit-msg}"
+      fixes=true
+    fi
+  done < bzs.raw
+}
+
+format_bz_for_md() {
+  local bz roles doc_text summary nf_file bf_file jq output fix_summary
+  bz="$1"
+  nf_file="$2"
+  bf_file="$3"
+  jq='.bugs[] | ((.whiteboard|gsub("role:"; "")) + "|" + .cf_doc_type + "|" + .summary)'
+  IFS=\| read -r roles doc_text summary <<< "$(bugzilla query -b "$bz" --json | jq -r "$jq")"
+  if [ "${doc_text:-}" = Enhancement ]; then
+    output="$nf_file"
+  elif [ "${doc_text:-}" = "Bug Fix" ]; then
+    output="$bf_file"
+  else
+    echo Skipping "$bz" "${doc_text:-no doc text}" "${summary:-no summary}"
+    return 0
+  fi
+  if [ -n "$roles" ]; then
+    if [[ "$summary" =~ ^${roles}[\ -]+(.+)$ ]]; then
+      fix_summary="$roles - ${BASH_REMATCH[1]}"
+    else
+      fix_summary="$roles - $summary"
+    fi
+  else
+    fix_summary="$summary"
+  fi
+  if [ -f "$output" ]; then
+    if grep -F -q "$fix_summary" "$output" || grep -q "\<${bz}\>" "$output"; then
+      # already in there - skip
+      return 0
+    fi
+  fi
+  echo "- [${fix_summary}](${show_url}$bz)" >> "$output"
+}
+
+make_cl_for_version() {
+  local version datestr cl_file cl_nf_file cl_bf_file
+  version="$1"
+  datestr="$2"
+  cl_file="$3"
+  cl_nf_file="$4"
+  cl_bf_file="$5"
+  if [ ! -f "$cl_nf_file" ] && [ ! -f "$cl_bf_file" ]; then
+    return 0  # no cl for this version
+  fi
+  { echo ""
+    echo ["$version"] - "$datestr"
+    echo ----------------------------
+    echo ""
+    echo "### New Features"
+    echo ""; } >> "$cl_file"
+  if [ -f "$cl_nf_file" ]; then
+    cat "$cl_nf_file" >> "$cl_file"
+  else
+    echo "- none" >> "$cl_file"
+  fi
+  { echo ""
+    echo "### Bug Fixes"
+    echo ""; } >> "$cl_file"
+  if [ -f "$cl_bf_file" ]; then
+    cat "$cl_bf_file" >> "$cl_file"
+  else
+    echo "- none" >> "$cl_file"
+  fi
+  rm -f "$cl_nf_file" "$cl_bf_file"
+}
+
+spec_cl_to_cl_md() {
+  local spec print cur_ver mon day year version datestr cl_file cl_nf_file cl_bf_file
+  spec="$1"
+  print=false
+  version=""
+  cur_ver=""
+  cl_file=".cl.md"
+  cl_nf_file=".cl-nf.md"
+  cl_bf_file=".cl-bf.md"
+  { echo Changelog
+    echo =========; } > "$cl_file"
+  while read -r line ; do
+    if [ "$line" = %changelog ]; then
+      print=true
+      continue
+    fi
+    if [ "$print" = false ]; then
+      continue
+    fi
+    if [[ "$line" =~ ^\*\ +[a-zA-Z]+\ +([a-zA-Z]+)\ +([0-9]+)\ +([0-9]+)\ .*-\ +([0-9]+\.[0-9]+(\.[0-9]+)?)- ]]; then
+      mon="${BASH_REMATCH[1]}"
+      day="${BASH_REMATCH[2]}"
+      year="${BASH_REMATCH[3]}"
+      version="${BASH_REMATCH[4]}"
+      need_new_datestr=false
+      if [ -n "$cur_ver" ] && [ "$version" != "$cur_ver" ]; then
+        make_cl_for_version "$cur_ver" "$datestr" "$cl_file" "$cl_nf_file" "$cl_bf_file"
+        need_new_datestr=true
+      fi
+      if [ -z "${datestr:-}" ] || [ "$need_new_datestr" = true ]; then
+        datestr=$(date --date="$mon $day $year" +%Y-%m-%d)
+      fi
+      cur_ver="$version"
+    elif [[ "$line" =~ bz.?([0-9]{7}) ]]; then
+      while [[ "$line" =~ bz.?([0-9]{7}) ]]; do
+        bz="${BASH_REMATCH[1]}"
+        format_bz_for_md "$bz" "$cl_nf_file" "$cl_bf_file"
+        line="${line/$bz}"
+      done
+    fi
+  done < "$spec"
+  make_cl_for_version "$cur_ver" "$datestr" "$cl_file" "$cl_nf_file" "$cl_bf_file"
+}
+
+"$@"

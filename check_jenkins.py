0344a42400057e9179cdc13ee26f2340c59a6c6d
Rich Megginson <rmeggins@redhat.com>
2022-10-25 19:31:45 -0600

    add support for token, cancel tasks, print_test_failures
    See README for details

--- check_jenkins.py
+++ check_jenkins.py
@@ -20,8 +20,12 @@ username = cfg[cfg["current"]]["username"]
 job_name = cfg[cfg["current"]]["job_name"]
 ssh_private_key = cfg[cfg["current"]].get("ssh_private_key")
 ssh_user = cfg[cfg["current"]].get("ssh_user")
+token = cfg[cfg["current"]].get("token")
 
-server = jenkins.Jenkins(url, username=username)
+kwargs = {"username": username}
+if token:
+    kwargs["password"] = token
+server = jenkins.Jenkins(url, **kwargs)
 job = server.get_job_info(job_name, depth=0, fetch_all_builds=False)
 # yaml.safe_dump(job, open("junk.yml", "w"))
 task_nums = [build["number"] for build in job["builds"]]
@@ -263,8 +267,8 @@ def print_completed_tasks(server, task_nums, args):
             print(format_fields(task, "completed", ts))
 
 
-def stop_tasks(server, task_nums, args):
-    """Stop tasks matching the display_name pattern."""
+def cancel_tasks(server, task_nums, args):
+    """Cancel given tasks."""
     for num in args:
         print(f"Stopping {num}")
         server.stop_build(job_name, num)
@@ -428,6 +432,62 @@ def print_node_info(server, task_nums, args):
 #         pass
 #     cmd = f"scp -i {ssh_private_key} {ssh_user}@{ip}:"
 #     subprocess.run(cmd)
+
+
+def cancel_duplicate_tasks(server, task_nums, args):
+    """Stop duplicate queued and running tasks.  Keep the newest one."""
+    found_keys = set()
+    queue_info = server.get_queue_info()
+    for task in queue_info:
+        if task["task"]["name"] == job_name:
+            label = get_pr_status_label(task, False)
+            role, prnum = get_pr_info(task)
+            key = f"{label}:{role}:{prnum}"
+            if key in found_keys:
+                tsstr = datetime.datetime.fromtimestamp(
+                    task["inQueueSince"] / 1000
+                ).isoformat(timespec="seconds")
+                print(f"Cancelling duplicate queued task {task['id']} {key} {tsstr}")
+                server.cancel_queue(task["id"])
+            else:
+                found_keys.add(key)
+    for task, ts in task_iter(task_nums, server):
+        if task["result"] is None:
+            label = get_pr_status_label(task, False)
+            role, prnum = get_pr_info(task)
+            key = f"{label}:{role}:{prnum}"
+            if key in found_keys:
+                tsstr = ts.isoformat(timespec="seconds")
+                print(f"Cancelling duplicate running task {task['id']} {key} {tsstr}")
+                server.stop_build(job_name, task["id"])
+            else:
+                found_keys.add(key)
+
+
+def print_test_failures(server, task_nums, args):
+    """Print test failures for tasks matching args."""
+    role, prnum, label = None, None, None
+    if len(args) > 0:
+        role = args[0]
+    if len(args) > 1:
+        prnum = args[1]
+    if len(args) > 2:
+        label = args[2]
+    for task, ts in task_iter(task_nums, server):
+        if task["result"] is None:
+            task_label = get_pr_status_label(task)
+            task_role, task_prnum = get_pr_info(task)
+            if (
+                role == task_role
+                and (prnum is None or prnum == task_prnum)
+                and (label is None or label == task_label)
+            ):
+                info = get_task_tests_info(server, int(task["id"]))
+                for test, data in info["tests"].items():
+                    if data["result"] not in ["PASSED", "UNDEFINED"]:
+                        print(
+                            f"{task_role} {task_prnum} {task_label} {test} {info['start']}"
+                        )
 
 
 if len(sys.argv) > 1:

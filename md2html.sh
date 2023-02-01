73800682a3293ef5ab5ed5880329ce792cd34bbf
Sergei Petrosian <30409084+spetrosi@users.noreply.github.com>
2023-02-01 16:20:45 +0100

    md2html.sh add functionality to add TOC to HTML (#242)
    * md2html.sh add functionality to add TOC to HTML
    
    * Remove quotes around optional argument variable to avoid empty argument

--- md2html.sh
+++ md2html.sh
@@ -3,16 +3,23 @@
 set -euxo pipefail
 
 convert_link=0
+add_toc=0
+toc_arg=""
 
-while getopts "hl" opt; do
+while getopts "htl" opt; do
   case "$opt" in
     h)
       echo "Usage: $0 [-h] [[-l] md_file0 md_file1 ...]"
       echo "options:"
       echo "-l  convert links to other md files to html"
+      echo "-t  add TOC to the converted HTML doc. Available for kramdown only."
       exit ;;
     l)
       convert_link=1
+      shift ;;
+    t)
+      add_toc=1
+      toc_arg='--toc-levels 2..6'
       shift ;;
     *)
       ;;
@@ -29,9 +36,18 @@ for file in "$@"; do
   fi
   # With kramdown, convert directly to HTML
   if [ "$md2html_tool" == kramdown ]; then
+    if [ "$add_toc" -ne 0 ]; then
+      sed -i '1s/^/* toc\n{:toc}\n/' "${file}"
+    fi
     # Set locale to UTF-8 because by default it is set to US-ASCII
-    LC_ALL=C.UTF-8 $md2html_tool --extension parser-gfm --input GFM \
-    --output html "${file}" > "${file%.md}.html"
+    # shellcheck disable=SC2086
+    LC_ALL=C.UTF-8 $md2html_tool --extension parser-gfm \
+      --input GFM \
+      $toc_arg \
+      --output html "${file}" > "${file%.md}.html"
+    if [ "$add_toc" -ne 0 ]; then
+      sed -i '1,2d' "${file}"
+    fi
   # With pandoc, convert to adoc, then to HTML
   elif [ "$md2html_tool" == pandoc ]; then
     $md2html_tool -f gfm "${file}" -t asciidoc -o "${file%.md}.tmp.adoc"
